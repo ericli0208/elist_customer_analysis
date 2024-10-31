7377f965ac38e56e5bfe9b025739c5faf52bda6b
Eric Li <ericli0208@gmail.com>
2024-10-31 15:16:26 -0700

    Update sql_analysis.sql

--- elist_analysis/sql_analysis.sql
+++ elist_analysis/sql_analysis.sql
@@ -1,9 +1,61 @@
+-- ELIST ANALYSIS - TARGETED BUSINESS QUESTIONS
+
+-- Return the id, loyalty program status, and account creation date for customers who made an account on desktop or mobile. Rename the columns to more descriptive names.
+SELECT id AS customer_id, 
+  loyalty_program AS is_loyalty_program, 
+  created_on AS account_creation_date
+FROM elistcore.core.customers
+WHERE account_creation_method IN ('desktop', 'mobile');
+
+-- What were the total number of orders placed using each currency?
+SELECT DISTINCT currency,
+  COUNT(id) as count_of_orders
+FROM elistcore.core.orders
+-- WHERE currency IS NOT NULL (why is there 54 orders with null currency?)
+GROUP BY 1
+ORDER BY 1;  
+
+-- What is the total number of orders by shipping month, sorted from most recent to oldest?
+SELECT DATE_TRUNC(ship_ts, MONTH) AS shipping_month,
+  COUNT(distinct order_id) AS num_orders
+FROM elistcore.core.order_status
+GROUP BY 1, 2
+ORDER BY 1 DESC;
+
+-- What is the average order value by year? Can you round the results to 2 decimals?
+SELECT EXTRACT(YEAR FROM purchase_ts) AS year, 
+  ROUND(AVG(usd_price), 2) AS aov
+FROM elistcore.core.orders
+GROUP BY 1
+ORDER BY 1;
+
+-- Create a helper column `is_refund`  in the `order_status`  table that returns 1 if there is a refund, 0 if not. Return the first 20 records.
+SELECT *, 
+  CASE WHEN refund_ts IS NOT NULL THEN 1 ELSE 0 END AS is_refund
+FROM core.order_status
+LIMIT 20;
+
+-- Return the product IDs and product names of all Apple products.
+SELECT product_id, product_name
+FROM elistcore.core.orders
+WHERE product_name IN ('Apple Airpods Headphones','Apple iPhone','Macbook Air Laptop');
+-- WHERE product_name LIKE '%Apple%' OR product_name = 'Macbook Air Laptop'
+
+-- Calculate the time to ship in days for each order and return all original columns from the table.
+SELECT *, 
+  DATE_DIFF(ship_ts,purchase_ts, day) AS days_to_ship
+FROM elistcore.core.order_status; 
+
+-- Which months have the highest average time to ship? 
+SELECT EXTRACT(month from os.purchase_ts),
+  ROUND(AVG(DATE_DIFF(os.ship_ts, os.purchase_ts, day)), 2) AS days_to_ship
+FROM elistcore.core.order_status os
+LEFT JOIN elistcore.core.orders o
+    ON os.order_id = o.id
+GROUP BY 1 
+ORDER BY 2 DESC; 
+
 -- What were the order counts, sales, and AOV for Macbooks sold in North America for each quarter across all years? 
-
--- fields: year + quarter (date_trunc purchase_ts, aggregates(count of order id, sum of usd price, average of usd price)
--- filters: product name LIKE macbook, region = NA
--- tables: orders, customers, geo_lookup
-
 SELECT EXTRACT(year from o.purchase_ts) as year,
   CONCAT("Q", EXTRACT(quarter from o.purchase_ts)) as quarter,
   COUNT(DISTINCT o.id) as order_count,
@@ -19,16 +71,7 @@ AND g.region= 'NA'
 GROUP BY 1, 2
 ORDER BY 1, 2;
 
--- ELIST ANALYSIS - BUSINESS QUESTIONS
-
 -- For products purchased in 2022 on the website or products purchased on mobile in any year, which region has the average highest time to deliver? 
-
--- fields: region, time to deliver (datediff of deliver ts - purchase ts)
--- first filter: year (extract) = 2022 and purchase_platform = website
--- second filter: purchase_platform = mobile
--- order by time to deliver desc
--- tables: order_status (main), order, customer
-
 SELECT g.region,
   AVG(DATE_DIFF(os.delivery_ts, os.purchase_ts, day)) as time_to_deliver
 FROM elistcore.core.order_status os
@@ -44,17 +87,12 @@ GROUP BY 1
 ORDER BY 2 DESC;
 
 -- What was the refund rate and refund count for each product overall?
-
--- fields: product_name, refunded (count(refund_ts), refund rate (avg of case when refund_ts is not null then 1 else 0)
--- no filters
--- tables: order_status and orders
-
 SELECT DISTINCT product_name
 FROM core.orders;
 
-SELECT CASE WHEN product_name = '27in"" 4k gaming monitor' THEN '27in 4K gaming monitor' ELSE product_name END AS product_name_cleaned, 
-  COUNT(refund_ts) as refunded,
-  AVG(CASE WHEN refund_ts IS NOT NULL THEN 1 ELSE 0 END) as refund_rate
+SELECT CASE WHEN o.product_name = '27in"" 4k gaming monitor' THEN '27in 4K gaming monitor' ELSE product_name END AS product_name_cleaned, 
+  COUNT(os.refund_ts) as refunded,
+  AVG(CASE WHEN os.refund_ts IS NOT NULL THEN 1 ELSE 0 END) as refund_rate
 FROM core.orders o
 LEFT JOIN core.order_status os
   ON o.id = os.order_id
@@ -62,13 +100,6 @@ GROUP BY 1
 ORDER BY 1;
 
 -- Within each region, what is the most popular product?
-
--- assuming popular = highest order_counts
--- we will need CTEs to break this down
--- CTE 1: count total orders per product
--- CTE 2: rank each product and region  by the total orders in a new CTE
--- need partition region and order by order_count
-
 WITH order_count_cte as (
   SELECT g.region,
     o.product_name,
@@ -90,7 +121,6 @@ FROM ranking_cte
 WHERE ranking = 1; 
 
 -- using qualify to filter using row_number without the second CTE 
-
 WITH order_count_cte as (
   SELECT g.region,
     o.product_name,
@@ -108,11 +138,6 @@ FROM order_count_cte
 QUALIFY row_number() over (PARTITION BY region ORDER BY order_count DESC) = 1;
 
 -- How does the time to make a purchase differ between loyalty customers vs. non-loyalty customers? 
-
--- time_to_purchase = datediff(account_creation_date, customer's first purchase ts) - both days and months
--- break loyalty_program into loyalty and non-loyalty with a case when
--- tables: customers, orders, order status
-
 SELECT CASE WHEN c.loyalty_program = 1 THEN 'loyalty' ELSE 'non-loyalty' END as loyalty,
   ROUND(AVG(DATE_DIFF(os.purchase_ts, c.created_on, day)), 1) as days_to_purchase,
   ROUND(AVG(DATE_DIFF(os.purchase_ts, c.created_on, month)), 1) as months_to_purchase
